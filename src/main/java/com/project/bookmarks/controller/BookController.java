0fcf7fd17879604c8c523fb57926f47707732b0d
Raymond Rivera <rraymondjohn@gmail.com>
2022-07-18 17:11:38 +0800

    V1.2

--- src/main/java/com/project/bookmarks/controller/BookController.java
+++ src/main/java/com/project/bookmarks/controller/BookController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path="/bookmarks/books")
 public class BookController {
     @Autowired
--- src/main/java/com/project/bookmarks/controller/UserController.java
+++ src/main/java/com/project/bookmarks/controller/UserController.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path="/bookmarks/users")
 public class UserController {
     @Autowired
